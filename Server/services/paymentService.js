adb67552bd25282cc39edffd66a75a8a07218a03
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2025-07-23 20:37:47 +0530

    use payment apis

--- Server/services/paymentService.js
+++ Server/services/paymentService.js
@@ -1,5 +1,10 @@
 const { Cashfree } = require("cashfree-pg");
 
+
+Cashfree.XClientId = process.env.CLIENT_ID
+Cashfree.XClientSecret = process.env.CLIENT_SECRET
+Cashfree.XEnvironment = Cashfree.Environment.PRODUCTION
+
 // ✅ Generate unique Order ID
 const generateOrderId = () => "ORD" + Date.now();
 
