9d46cc213849656c962457d42f49a5d86d1e4f47
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2025-07-23 14:21:03 +0530

    change folders

--- src/components/FootItem.js
+++
@@ -1,4 +0,0 @@
-export const navItems = [
-  { label: "Home", key: "home" },
-  { label: "About", key: "about" },
-];
--- src/components/NavItem.js
+++
@@ -1,5 +0,0 @@
-export const navItems = [
-  { label: "Home", key: "home" },
-  { label: "About", key: "about" },
-  { label: "Cart", key: "cart" },
-];
