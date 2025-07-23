dda1266c3f712bd20e8fc4ac90870e0cff73f610
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2025-07-24 00:05:46 +0530

    orders page fix 2.0

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -4,7 +4,7 @@ const routes = (data) => {
     data.post('/api/orders', orderCreate);
     data.get('/api/orders', orderList)
     data.patch('/api/orders/:id', orderUpdate);   
-    data.get('/api/orders/user/:id',userOrderList) 
+   data.get('/api/orders/user/:id', userOrderList)
 }
 
 module.exports = routes
